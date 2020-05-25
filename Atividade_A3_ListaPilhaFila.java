82986cee52f9cc35600b137f1b82fa393ce9cf5c
EricksonCeratti <30564533+EricksonCeratti@users.noreply.github.com>
2020-05-25 19:42:28 -0300

    Add files via upload

---
+++ Atividade_A3_ListaPilhaFila.java
@@ -0,0 +1,74 @@
+package atividade_a3_lista.pilha.fila;
+
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+
+/**
+ *
+ * @author Erickson
+ */
+public class Atividade_A3_ListaPilhaFila {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList lista = new ArrayList(5);
+        ArrayDeque pilha = new ArrayDeque(5);
+        ArrayDeque fila = new ArrayDeque(10);
+        
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
+        
+        System.out.println(lista + " - Números inseridos na Lista.");       
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||");
+        while(!lista.isEmpty()){
+            Object elemento = lista.remove(0);
+            pilha.add(elemento);
+        System.out.println(lista + " - Dados removidos da Lista para inserir na Pilha");    
+        }
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+        System.out.println(pilha + " - Dados inseridos na Pilha"); 
+        while(!pilha.isEmpty()){
+            Object elemento = pilha.pop();
+            fila.add(elemento);
+        System.out.println(pilha + " - Dados removidos da Pilha para inserir na Fila");
+        
+        }
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+        System.out.println(fila + " - Dados inseridos na Fila");
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+        
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(9);
+        lista.add(10);
+        
+        System.out.println(lista + " - Numeros inseridos na lista");
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+        
+        while(!lista.isEmpty()){
+            Object elemento = lista.remove(0);
+            pilha.add(elemento);
+        System.out.println(lista + " - Dados removidos da Lista para inserir na Pilha");
+        
+
+        }
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+        System.out.println(pilha + " - Dados inseridos na Pilha");
+        
+        while(!pilha.isEmpty()){
+            Object elemento = pilha.pop();
+            fila.add(elemento);
+        System.out.println(pilha + " - Dados removidos da Pilha para inserir na Fila");
+                       
+        }
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+        System.out.println(fila + " - Dados inseridos na Fila");
+    }
+    
+}
